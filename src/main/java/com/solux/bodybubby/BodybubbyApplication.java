a8d0d30fdf020dd3c7416c99c799002e3ec03a99
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-11-13 20:14:57 +0900

    [feat] DB 설정

--- src/main/java/com/solux/bodybubby/BodybubbyApplication.java
+++ src/main/java/com/solux/bodybubby/BodybubbyApplication.java
@@ -2,14 +2,9 @@ package com.solux.bodybubby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // <--- 이 부분 추가
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration; // <--- 이 부분 추가
 
 // @SpringBootApplication 어노테이션에 DB 관련 자동 설정을 제외하는 옵션을 추가합니다.
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class BodybubbyApplication {
 
 	public static void main(String[] args) {
