3f9cdc585aca124208cca73ec1932e2c0fe1f9dc
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-11-13 18:37:28 +0900

    [feat] 초기 프로젝트 세팅
    DB 설정 필요

---
+++ src/test/java/com/solux/bodybubby/BodybubbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solux.bodybubby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodybubbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
