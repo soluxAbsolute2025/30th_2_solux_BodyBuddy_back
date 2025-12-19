a5404c07eab21e49ed6c99ae637c46c9d1f15d83
lovekukwithhan <skylar4790@naver.com>
2025-12-20 02:05:25 +0900

    feat: setup user repository and controller for google login

--- src/main/java/com/solux/bodybubby/domain/user/repository/UserRepository.java
+++ src/main/java/com/solux/bodybubby/domain/user/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 소셜 로그인 시 이메일로 기존 회원을 찾기 위한 메서드입니다.
+    // 소셜 로그인 시 이메일로 기존 회원을 찾기 위한 메서드
     Optional<User> findByEmail(String email);
 }
