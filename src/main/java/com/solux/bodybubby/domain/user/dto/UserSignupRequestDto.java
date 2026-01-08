f3cc219288e683eb5799f652129057af827ac155
Kim-Jeongmin <169152237+min1i@users.noreply.github.com>
2026-01-09 04:29:08 +0900

    #5 [feat] 회원가입/온보딩 DTO 구현 및 UserRepository 생성

--- src/main/java/com/solux/bodybubby/domain/user/dto/UserSignupRequestDto.java
+++ src/main/java/com/solux/bodybubby/domain/user/dto/UserSignupRequestDto.java
@@ -1,18 +1,24 @@
 package com.solux.bodybubby.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class UserSignupRequestDto {
+
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,12}$", message = "아이디는 4~12자의 영문 소문자 및 숫자 조합이어야 합니다.") //
     private String loginId;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d|.*\\W).{6,20}$", message = "비밀번호는 6~20자의 영문 대소문자, 숫자, 특수문자 중 2가지 이상 조합이어야 합니다.") //
     private String password;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
-    private String nickname; // 닉네임
-    private List<String> mainGoals;    // 주요 목표 (수분, 식단, 약 등)
-    private String privacyScope;       // 허용 범위 (수분만 공유 등)
-    private String referrerId;         // 추천인 아이디
 }
