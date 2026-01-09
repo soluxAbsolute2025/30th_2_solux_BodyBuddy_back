b13bc18daa9c6a5afde0a3a49580792da40f8bda
Kim-Jeongmin <169152237+min1i@users.noreply.github.com>
2026-01-10 01:20:30 +0900

    #5 [feat] 로그인 ID 및 닉네임 중복 확인을 위한 Repository 메서드 추가

--- src/main/java/com/solux/bodybubby/domain/user/repository/UserRepository.java
+++ src/main/java/com/solux/bodybubby/domain/user/repository/UserRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 소셜 로그인 시 이메일로 기존 회원을 찾기 위한 메서드
-    Optional<User> findByEmail(String email);
+    Optional<User> findByLoginId(String loginId);
 
-    // 닉네임 중복확인
+    boolean existsByLoginId(String loginId);
+
     boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
