9f31f410a944ad707f8790651256ea779fe80c3e
Kim-Jeongmin <169152237+min1i@users.noreply.github.com>
2026-01-12 16:25:33 +0900

    #5 [feat] 스프링 시큐리티 유저 인증 기반 구축

---
+++ src/main/java/com/solux/bodybubby/global/security/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package com.solux.bodybubby.global.security;
+
+import com.solux.bodybubby.domain.user.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private final User user; //
+    private final Long id;   // 유빈 님이 강조하신 Long id 필드
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+        this.id = user.getId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    // 계정 상태 (일단 모두 true)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+}
