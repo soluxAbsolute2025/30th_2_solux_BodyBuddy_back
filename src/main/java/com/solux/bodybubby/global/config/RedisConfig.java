ada24b5c46ca245aaa6f2b2eb020e3b0399a086e
Kim-Jeongmin <169152237+min1i@users.noreply.github.com>
2026-01-16 05:14:16 +0900

    #5 [feat] RedisTemplate에 String 직렬화 설정 추가

--- src/main/java/com/solux/bodybubby/global/config/RedisConfig.java
+++ src/main/java/com/solux/bodybubby/global/config/RedisConfig.java
@@ -6,10 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories // Redis Repository 활성화
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -27,6 +26,11 @@ public class RedisConfig {
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        // 레디스 키와 값을 문자열로 읽을 수 있게 직렬화 설정을 추가합니다.
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
